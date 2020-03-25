361bea599bd57b7e8ed8b4eac260e9fca469a430
Singhal <rishabh.singhal@cybergindia.local>
2020-03-26 01:37:49 +0530

    Updated Code

---
+++ MVC_Application/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using MVC_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Application.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customer = GetCustomers();
+            return View(customer);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(cust => cust.Id == id);
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer{Id=1,Name="Steve Jobs"},
+                new Customer{Id=2,Name="Bill Gates"}
+            };
+        }
+    }
+}
---
+++ MVC_Application/Controllers/MoviesController.cs
@@ -0,0 +1,62 @@
+﻿using MVC_Application.Models;
+using MVC_Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Application.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random(int id)
+        {
+            var movie = new Movie() { Name = "Avengers" , Id=id};
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"},
+                new Customer {Name = "Customer 3"},
+                new Customer {Name = "Customer 4"},
+                new Customer {Name = "Customer 5"}
+            };
+
+            var viewModel = new RandomMovieViewModel { Movie=movie, Customers=customers };
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id="+id);
+        }
+
+        public ActionResult Index(int? pageNumber, string sortBy)
+        {
+            //if (!pageNumber.HasValue)
+            //    pageNumber = 1;
+            //if (String.IsNullOrWhiteSpace(sortBy))
+            //    sortBy = "Date";
+            //return Content(String.Format("PageNumber={0} & SortBy={1}", pageNumber, sortBy));
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        [Route("movies/release/{year}/{month:range(1, 12)}")]
+        public ActionResult Release(int year, int month)
+        {
+            return Content(year+"/"+month);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id=1, Name="Time Trap"},
+                new Movie{Id=2, Name="Intersteller"}
+            };
+        }
+    }
+
+}
